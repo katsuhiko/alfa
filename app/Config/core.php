b0ba39a9575c9f7e21deb5bf179bc072ac737867
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2013-10-05 23:21:33 +0900

    CakePHPの初期動作確認

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '498828cae59b59cbeebb83b7e1dae6135bdfdda6');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '297915548049696203318970070117');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
